393c40108b68910172f5a9acfde649fa0623e170
Brandon Buchs <bbuchs@wisc.edu>
2020-08-10 21:56:55 -0400

    Updates

---
+++ precache-manifest.074ec80a45425fa95f6e382af3ebaac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e812aad41edefd74f2adfb7bb518390",
+    "url": "/778-final-portfolio/index.html"
+  },
+  {
+    "revision": "14a29096912679a39f64",
+    "url": "/778-final-portfolio/static/css/main.16a58bf5.chunk.css"
+  },
+  {
+    "revision": "c8f469e36e34d2a74dd9",
+    "url": "/778-final-portfolio/static/js/2.b455aca6.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/778-final-portfolio/static/js/2.b455aca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a29096912679a39f64",
+    "url": "/778-final-portfolio/static/js/main.47ee813e.chunk.js"
+  },
+  {
+    "revision": "f2792e42421aaef3bb46",
+    "url": "/778-final-portfolio/static/js/runtime-main.e953202b.js"
+  }
+]);
